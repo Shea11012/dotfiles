77087263d493d2c4198b7e01e0263f3c1469de48
mxy <mxyinvoker@gmail.com>
2024-09-25 14:01:34 +0800

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -32,7 +32,7 @@ if status is-interactive
   # alias
   alias ls="lsd"
   alias ll="lsd -la"
-  alias lt="lsd -a --tree"
+  alias lt="ll --depth 2"
   alias rime-deploy='qdbus6 org.fcitx.Fcitx5 /controller org.fcitx.Fcitx.Controller1.SetConfig "fcitx://config/addon/rime/deploy" ""'
   alias rime-sync='qdbus6 org.fcitx.Fcitx5 /controller org.fcitx.Fcitx.Controller1.SetConfig "fcitx://config/addon/rime/sync" ""'
 end
