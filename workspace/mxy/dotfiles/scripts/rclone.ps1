9e478f191f28d7efabb0e3a05580aa340ac0d062
mxy <mxyinvoker@gmail.com>
2025-02-28 19:10:10 +0800

    Update workspace/mxy/dotfiles/scripts/rclone.ps1

--- workspace/mxy/dotfiles/scripts/rclone.ps1
+++ workspace/mxy/dotfiles/scripts/rclone.ps1
@@ -1,15 +1,21 @@
 # schtasks /create /tn "rclone" /tr "powershell.exe -WindowStyle hidden -Command rclone mount alist: Z: --cache-dir D:\alist-cache --vfs-cache-mode writes --local-no-sparse"  /sc onlogon /f
 
-$targetDir = "D:alist-cache"
+$targetDir = "D:\alist-cache"
+$logFile = $targetDir + "\rclone.log"
+
+if (Test-Path -Path $logFile) {
+    Remove-Item $logFile
+}
+
 if (-not (Test-Path -Path $targetDir -PathType Container)) {
     New-Item -ItemType Directory -Path $targetDir | Out-Null
 }
 
 rclone mount alist: Z: `
-    --cache-dir 'D:\alist-cache' `
+    --cache-dir $targetDir `
     --vfs-cache-mode writes `
     --local-no-sparse `
-    --log-file 'D:\alist-cache\rclone.log' `
+    --log-file $logFile `
     --log-level INFO `
     # --volname "Alist云存储" `
     # --allow-other
