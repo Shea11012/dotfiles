8c897c1c793636033644853dd85dd022ec56c6a2
mxy <mxyinvoker@gmail.com>
2025-05-09 14:48:27 +0800

    Remove .config/nvim/lua/plugins/vscode.lua

--- private_dot_config/nvim/lua/plugins/vscode.lua
+++
@@ -1,9 +0,0 @@
-if not vim.g.vscode then return {} end
-
-return {
-  "AstroNvim/astrocore",
-  opts = function(_, opts)
-    local maps = opts.mappings
-    maps.n["<Leader>ff"] = function() require("vscode-neovim").action "actions.find" end
-  end,
-}
