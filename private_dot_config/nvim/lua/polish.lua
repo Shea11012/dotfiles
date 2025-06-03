46600ae6dba133077b9ceaa92027da4330c6c44e
mxy <mxyinvoker@gmail.com>
2025-06-03 15:33:31 +0800

    Update .config/nvim/lua/polish.lua Update workspace/mxy/dotfiles/clash/config.tp.yaml Update workspace/mxy/dotfiles/justfile

--- private_dot_config/nvim/lua/polish.lua
+++ private_dot_config/nvim/lua/polish.lua
@@ -2,24 +2,41 @@
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 local function paste()
-    return {
-        vim.fn.split(vim.fn.getreg "", "\n"),
-        vim.fn.getregtype "",
-    }
+  return {
+    vim.fn.split(vim.fn.getreg "", "\n"),
+    vim.fn.getregtype "",
+  }
 end
 
 -- 使用OSC52可以将剪贴板内容复制到本地
 if vim.env.SSH_TTY then
-    vim.o.clipboard = "unnamedplus"
-    vim.g.clipboard = {
-        name = "OSC 52",
-        copy = {
-            ["+"] = require("vim.ui.clipboard.osc52").copy "+",
-            ["*"] = require("vim.ui.clipboard.osc52").copy "*",
-        },
-        paste = {
-            ["+"] = paste,
-            ["*"] = paste,
-        },
-    }
+  vim.o.clipboard = "unnamedplus"
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+      ["*"] = require("vim.ui.clipboard.osc52").copy "*",
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+  }
+end
+
+if vim.fn.executable "pwsh" == 1 then
+  vim.o.shell = "pwsh"
+  -- Setting shell command flags
+  vim.o.shellcmdflag =
+    "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+
+  -- Setting shell redirection
+  vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+
+  -- Setting shell pipe
+  vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
+
+  -- Setting shell quote options
+  vim.o.shellquote = ""
+  vim.o.shellxquote = ""
 end
