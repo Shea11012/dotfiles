824796cd9fbc95fb0abd6d475eec64a912f875d9
mxy <mxyinvoker@gmail.com>
2025-07-29 10:42:10 +0800

    Update .config/fontconfig/fonts.conf Update .config/mise/config.toml Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -23,22 +23,8 @@ if status is-interactive
   starship init fish | source
   uv generate-shell-completion fish | source
 
-  # 配置全局变量
-  set -g fish_greeting
-  set -gx EDITOR "nvim"
-  set -gx RUSTUP_DIST_SERVER "https://rsproxy.cn"
-  set -gx RUSTUP_UPDATE_ROOT "https://rsproxy.cn/rustup"
-  set -gx MANPAGER "nvim +Man!"
-
   if not is_wsl
     # set -gx DOCKER_HOST unix://$XDG_RUNTIME_DIR/docker.sock
     set -gx BROWSER librewolf
   end
-
-  # alias
-  alias ls="lsd"
-  alias ll="lsd -la"
-  alias lt="lsd -a --tree"
-  alias rime-deploy='qdbus6 org.fcitx.Fcitx5 /controller org.fcitx.Fcitx.Controller1.SetConfig "fcitx://config/addon/rime/deploy" ""'
-  alias rime-sync='qdbus6 org.fcitx.Fcitx5 /controller org.fcitx.Fcitx.Controller1.SetConfig "fcitx://config/addon/rime/sync" ""'
 end
