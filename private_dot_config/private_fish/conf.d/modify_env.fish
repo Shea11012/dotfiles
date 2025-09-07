3ccd9a64bc34a6e70d7c01258cf8ca22a3a3144f
mxy <mxyinvoker@gmail.com>
2025-09-07 17:10:56 +0800

    Add .config/fish/conf.d/env.fish Remove .config/fish/conf.d/env.fish Add .config/fish/conf.d/private.fish

--- private_dot_config/private_fish/conf.d/modify_env.fish
+++
@@ -1,8 +0,0 @@
-# 配置全局变量
-set -g fish_greeting
-set -gx EDITOR "nvim"
-set -gx RUSTUP_DIST_SERVER "https://rsproxy.cn"
-set -gx RUSTUP_UPDATE_ROOT "https://rsproxy.cn/rustup"
-set -gx MANPAGER "nvim +Man!"
-{{- /* chezmoi:modify-template */ -}}
-{{ .chezmoi.stdin | replaceAllRegex "set -x GithubToken .*" (printf "set -x GithubToken %s" (fromToml (joinPath .chezmoi.sourceDir "workspace/mxy/dotfiles/encrypted_env.toml.age" | include | decrypt)).GithubToken | toString )}}
