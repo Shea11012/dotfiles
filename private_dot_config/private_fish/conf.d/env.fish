0a1d26f85a93d87bbc02a28d2e2b059ec251f61a
mxy <mxyinvoker@gmail.com>
2025-09-07 17:18:06 +0800

    Update .config/fish/conf.d/env.fish Update .config/fish/conf.d/private.fish

--- private_dot_config/private_fish/conf.d/env.fish
+++ private_dot_config/private_fish/conf.d/env.fish
@@ -4,3 +4,5 @@ set -gx EDITOR "nvim"
 set -gx RUSTUP_DIST_SERVER "https://rsproxy.cn"
 set -gx RUSTUP_UPDATE_ROOT "https://rsproxy.cn/rustup"
 set -gx MANPAGER "nvim +Man!"
+set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
+
