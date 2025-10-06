136878c5562bf3051dc61a0cea8cccd6278656f7
mxy <mxyinvoker@gmail.com>
2025-10-07 03:01:11 +0800

    Update .gitconfig Update .config/nvim/lua/plugins/astrocore.lua Update .config/nvim/lua/polish.lua Remove workspace/mxy/dotfiles/rules/sub-store/convert.js

--- private_dot_config/nvim/lua/polish.lua
+++ private_dot_config/nvim/lua/polish.lua
@@ -2,70 +2,70 @@
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 local function paste()
-	return {
-		vim.fn.split(vim.fn.getreg(""), "\n"),
-		vim.fn.getregtype(""),
-	}
+  return {
+    vim.fn.split(vim.fn.getreg "", "\n"),
+    vim.fn.getregtype "",
+  }
 end
 
 -- 使用OSC52可以将剪贴板内容复制到本地
 if vim.env.SSH_TTY then
-	vim.o.clipboard = "unnamedplus"
-	vim.g.clipboard = {
-		name = "OSC 52",
-		copy = {
-			["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-			["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-		},
-		paste = {
-			["+"] = paste,
-			["*"] = paste,
-		},
-	}
+  vim.o.clipboard = "unnamedplus"
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+      ["*"] = require("vim.ui.clipboard.osc52").copy "*",
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+  }
 end
 
 local nu_shell_options = {
-	sh = "nu",
-	shellslash = true,
-	shellcmdflag = "--stdin --no-newline -c",
-	shellpipe = "| complete | update stderr { ansi strip } | tee { get stderr | save --froce --raw %s } | into record",
-	shellquote = "",
-	shellredir = "out+err> %s",
-	shelltemp = false,
-	shellxescape = "",
-	shellxquote = "",
+  sh = "nu",
+  shellslash = true,
+  shellcmdflag = "--stdin --no-newline -c",
+  shellpipe = "| complete | update stderr { ansi strip } | tee { get stderr | save --froce --raw %s } | into record",
+  shellquote = "",
+  shellredir = "out+err> %s",
+  shelltemp = false,
+  shellxescape = "",
+  shellxquote = "",
 }
 
 local function set_options(options)
-	for k, v in pairs(options) do
-		vim.opt[k] = v
-	end
+  for k, v in pairs(options) do
+    vim.opt[k] = v
+  end
 end
 
-if vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 then
-	set_options(nu_shell_options)
-	-- if vim.o.shell:find("bash", 1) then vim.o.shellcmdflag = "-s" end
-	--   vim.o.shell = "nu"
-	--   -- Setting shell command flags
-	--   vim.o.shellcmdflag = ""
-	--
-	--   -- Setting shell redirection
-	--   vim.o.shellredir = ""
-	--
-	--   -- Setting shell pipe
-	--   vim.o.shellpipe = ""
-	--
-	--   -- Setting shell quote options
-	--   vim.o.shellquote = ""
-	--   vim.o.shellxquote = ""
-	--   -- vim.o.shellslash = true
+if vim.fn.has "win64" == 1 or vim.fn.has "win32" == 1 then
+  set_options(nu_shell_options)
+  -- if vim.o.shell:find("bash", 1) then vim.o.shellcmdflag = "-s" end
+  --   vim.o.shell = "nu"
+  --   -- Setting shell command flags
+  --   vim.o.shellcmdflag = ""
+  --
+  --   -- Setting shell redirection
+  --   vim.o.shellredir = ""
+  --
+  --   -- Setting shell pipe
+  --   vim.o.shellpipe = ""
+  --
+  --   -- Setting shell quote options
+  --   vim.o.shellquote = ""
+  --   vim.o.shellxquote = ""
+  --   -- vim.o.shellslash = true
 end
 
 vim.on_key(function(char)
-	if vim.fn.mode() == "n" then
-		local new_hlsearch = vim.tbl_contains({ "<CR>", "n", "N", "*", "#", "?", "/" }, vim.fn.keytrans(char))
-		if vim.opt.hlsearch ~= new_hlsearch then
-			vim.opt.hlsearch = new_hlsearch
-		end
-	end
-end, vim.api.nvim_create_namespace("auto_hlsearch"))
+  if vim.fn.mode() == "n" then
+    local new_hlsearch = vim.tbl_contains({ "<CR>", "n", "N", "*", "#", "?", "/" }, vim.fn.keytrans(char))
+    if vim.opt.hlsearch ~= new_hlsearch then vim.opt.hlsearch = new_hlsearch end
+  end
+end, vim.api.nvim_create_namespace "auto_hlsearch")
+
+vim.opt.iskeyword:append "-"
