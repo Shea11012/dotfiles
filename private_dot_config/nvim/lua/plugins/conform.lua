1494ec99856517763f935f8640ed13f4e9d536b1
mxy <mxyinvoker@gmail.com>
2026-03-28 12:21:59 +0800

    Update .config/niri/config.kdl Update .config/niri/window.kdl Update .config/nvim/lua/plugins/conform.lua Update .config/nvim/lua/plugins/user.lua Update .config/mpv/record-playlist.json Update .config/waybar/config.jsonc

--- private_dot_config/nvim/lua/plugins/conform.lua
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -23,9 +23,9 @@ return {
     opts.formatters_by_ft["rust"] = { "rustfmt" }
     opts.formatters_by_ft["python"] = {"ruff_format"}
 
-    -- opts.format_on_save = {
-    --   timeout_ms = 500,
-    --   lsp_format = "fallback",
-    -- }
+    opts.format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    }
   end,
 }
--- private_dot_config/nvim/lua/plugins/user.lua
+++ private_dot_config/nvim/lua/plugins/user.lua
@@ -21,20 +21,38 @@ return {
     "folke/snacks.nvim",
     opts = {
       dashboard = {
-        preset = {
-          header = table.concat({
-            " █████  ███████ ████████ ██████   ██████ ",
-            "██   ██ ██         ██    ██   ██ ██    ██",
-            "███████ ███████    ██    ██████  ██    ██",
-            "██   ██      ██    ██    ██   ██ ██    ██",
-            "██   ██ ███████    ██    ██   ██  ██████ ",
-            "",
-            "███    ██ ██    ██ ██ ███    ███",
-            "████   ██ ██    ██ ██ ████  ████",
-            "██ ██  ██ ██    ██ ██ ██ ████ ██",
-            "██  ██ ██  ██  ██  ██ ██  ██  ██",
-            "██   ████   ████   ██ ██      ██",
-          }, "\n"),
+        preset = {},
+        sections = {
+          function()
+            return {
+              header = require("modules.dashboard").header,
+              pane = 1,
+              padding = 1,
+            }
+          end,
+          { pane = 1, section = "startup", padding = 1 },
+          { pane = 1, section = "keys", padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "RECENT FILES",
+            section = "recent_files",
+            indent = 2,
+            padding = 1,
+          },
+          { pane = 2, icon = "󰙅 ", title = "PROJECTS", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "GIT STATUS [" .. vim.fn.trim(vim.fn.system "git branch --show-current") .. "]",
+            section = "terminal",
+            enabled = function() return Snacks.git.get_root() ~= nil end,
+            cmd = "git --no-pager diff --stat -B -M -C && git status --short --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 2,
+          },
         },
       },
     },
