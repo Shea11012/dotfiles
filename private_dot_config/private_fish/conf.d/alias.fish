10c4d9b66c2e286d0f7c50af68d90a4ec706f806
mxy <mxyinvoker@gmail.com>
2026-07-12 15:06:32 +0800

    Remove .config/eza/theme.yml Remove .config/fontconfig/conf.d/52-replace.conf Remove .config/fontconfig/fonts.conf Remove .config/gitui/theme.ron Remove .config/lazygit/config.yml Remove .config/mise/config.toml Remove .config/mouseless/config.yaml Remove .config/neovide/config.toml Remove .config/niri/config.kdl Remove .config/niri/custom/bind.kdl Remove .config/niri/custom/spawn.kdl Remove .config/niri/custom/window.kdl Remove .config/nushell/config.nu Remove .config/nushell/env.nu Remove .config/nvim/README.md Remove .config/nvim/.neoconf.json Remove .config/nvim/.stylua.toml Remove .config/nvim/init.lua Remove .config/nvim/lua/community.lua Remove .config/nvim/lua/lazy_setup.lua Remove .config/nvim/lua/modules/dashboard.lua Remove .config/nvim/lua/overseer/template/user/caddy.lua Remove .config/nvim/lua/overseer/template/user/js.lua Remove .config/nvim/lua/plugins/astrocore.lua Remove .config/nvim/lua/plugins/astrolsp.lua Remove .config/nvim/lua/plugins/astroui.lua Remove .config/nvim/lua/plugins/conform.lua Remove .config/nvim/lua/plugins/diagnostic.lua Remove .config/nvim/lua/plugins/go.lua Remove .config/nvim/lua/plugins/mason.lua Remove .config/nvim/lua/plugins/neovide.lua Remove .config/nvim/lua/plugins/nvim-lint.lua Remove .config/nvim/lua/plugins/rust.lua Remove .config/nvim/lua/plugins/treesitter.lua Remove .config/nvim/lua/plugins/user.lua Remove .config/nvim/lua/polish.lua Remove .config/nvim/lua/snippets/.keep Remove .config/nvim/lua/utils/im-switch.lua Remove .config/nvim/neovim.yml Remove .config/nvim/selene.toml Remove .config/fish/conf.d/alias.fish Remove .config/fish/conf.d/custom.fish Remove .config/fish/conf.d/env.fish Remove .config/fish/config.fish Remove .config/fish/fish_plugins Remove .config/jj/config.toml Remove .config/mpv/record-playlist.json Remove .config/mpv/input.conf Remove .config/mpv/justfile Remove .config/mpv/mpv.conf Remove .config/mpv/scripts/bookmarks.lua Remove .config/mpv/scripts/record.js Remove .config/resticprofile/key Remove .config/resticprofile/excludes.txt Remove .config/resticprofile/profiles.toml Remove .config/satty/config.toml Remove .config/starship.toml Remove .config/swaylock/config Remove .config/waybar/config.jsonc Remove .config/waybar/style.css Remove .config/wezterm/wezterm.lua Remove .config/yazi/init.lua Remove .config/yazi/keymap.toml Remove .config/yazi/package.toml Remove .config/yazi/theme.toml Remove .config/yazi/yazi.toml

--- private_dot_config/private_fish/conf.d/alias.fish
+++
@@ -1,3 +0,0 @@
-# alias
-alias rime-deploy='qdbus6 org.fcitx.Fcitx5 /controller org.fcitx.Fcitx.Controller1.SetConfig fcitx://config/addon/rime/deploy ""'
-alias rime-sync='qdbus6 org.fcitx.Fcitx5 /controller org.fcitx.Fcitx.Controller1.SetConfig fcitx://config/addon/rime/sync ""'
